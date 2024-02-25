ee6f00cae4cacb370fd2f94df87f449d8287da7b
eugenmihailescux@gmail.com <eugenmihailescux@gmail.com>
2024-02-25 03:29:34 +0100

    added authentication support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // see https://github.com/kelektiv/node.bcrypt.js/issues/979#issuecomment-1949878830
+     webpack: (config) => {
+    config.externals = [...config.externals, "bcrypt"];
+    return config;
+  }
+};
 
 module.exports = nextConfig;
--- scripts/seed.js
+++ scripts/seed.js
@@ -5,7 +5,7 @@ const {
   revenue,
   users,
 } = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
+
 
 async function seedUsers(client) {
   try {
@@ -25,6 +25,7 @@ async function seedUsers(client) {
     // Insert data into the "users" table
     const insertedUsers = await Promise.all(
       users.map(async (user) => {
+        const bcrypt = require('bcrypt');
         const hashedPassword = await bcrypt.hash(user.password, 10);
         return client.sql`
         INSERT INTO users (id, name, email, password)
